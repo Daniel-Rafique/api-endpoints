ad774eb5d3080d0e189419091e6a1b8a049f0576
Daniel-Rafique <drafique@tfd.ie>
2024-07-24 20:18:41 +0100

    PM2 inside correct directory

--- instanceInitializer.js
+++ instanceInitializer.js
@@ -45,7 +45,7 @@ class InstanceInitializer {
                 console.error('Failed to install dependencies:', installError);
                 return;
             }
-            exec(`pm2 start dist/index.js --name market-maker-${chatId}`, { cwd: userDir }, (pm2Error) => {
+            exec(`cd ${userDir} && pm2 start dist/index.js --name market-maker-${chatId}`, (pm2Error) => {
                 if (pm2Error) {
                     console.error('Failed to start market maker instance with PM2:', pm2Error);
                 }
