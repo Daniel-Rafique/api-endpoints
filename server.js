6381e2801b523bde8634b69c039bd909dc554595
Daniel-Rafique <drafique@tfd.ie>
2024-07-29 10:54:26 +0100

    Get instances

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ const walletWorker = new Worker('walletQueue', async job => {
     try {
         const wallets = walletManager.createSolanaWallets(makers);
         await walletManager.saveWallets(chatId, wallets);
-        await instanceInitializer.initializeMarketMakerInstance(chatId, boostType, makers, contractAddress);
+        await instanceInitializer.initializeMarketMakerInstance(chatId);
         console.log(`Processed job for chatId: ${chatId}`);
     } catch (error) {
         console.error('Error processing job:', error);
