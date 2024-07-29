3dba815c0c9d80098f1b63a068323ad1d0679ab3
Daniel-Rafique <drafique@tfd.ie>
2024-07-29 10:46:11 +0100

    Get instances

--- worker/wallet.js
+++ worker/wallet.js
@@ -1,21 +1,19 @@
 const { Worker } = require('bullmq');
 const WalletManager = require('../walletManager');
-const MarketMakerManager = require('../marketMakerManager');
 const InstanceInitializer = require('../instanceInitializer');
 
 class WalletWorker {
     constructor(queueName = 'walletQueue', connectionOptions = { host: 'localhost', port: 6379 }) {
         this.walletManager = new WalletManager('koynlabs-2f749', '.config/firebaseServiceAccountKey.json');
-        this.marketMakerManager = new MarketMakerManager('./marketMaker', './instances');
         this.instanceInitializer = new InstanceInitializer('./marketMaker', './instances');
 
         this.worker = new Worker(queueName, async job => {
-            const { chatId, boostType, count, contractAddress } = job.data;
+            const { chatId, boostType, makers, contractAddress } = job.data;
 
             try {
                 const wallets = this.walletManager.createSolanaWallets(count);
                 await this.walletManager.saveWallets(chatId, boostType, wallets);
-                await this.instanceInitializer.initializeMarketMakerInstance(chatId, boostType, count, contractAddress);
+                await this.instanceInitializer.initializeMarketMakerInstance(chatId, boostType, makers, contractAddress);
                 console.log(`Processed job for chatId: ${chatId}`);
             } catch (error) {
                 console.error('Error processing job:', error);
