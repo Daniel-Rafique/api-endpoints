61f0f25125942028cad8989c472f29fb3800100e
Daniel-Rafique <drafique@tfd.ie>
2024-07-31 12:02:04 +0100

    Add logging to class

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Connection, PublicKey, Transaction, SystemProgram, Keypair } = require('@solana/web3.js');
 const bs58 = require('bs58');
 const cron = require('node-cron');
-const WalletProcessor = require('../WalletProcessor'); 
+const WalletProcessor = require('../WalletProcessor');
 const DataManager = require('../database');
-const tokenProgramId = process.env.TOKEN_PROGRAM_ID;
+const tokenProgramId = process.env.TOKEN_PROGRAM_ID || 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA';
 const interval = process.env.CRON_JOB_INTERVAL || "*/1 * * * *"; 
-// const interval = "0 0 * * *";
+// const interval = "0 0 * * *"; // Uncomment for daily check
 
 class BalanceChecker {
   constructor(rpcEndpoints, telegramNotifier, walletASecretKey) {
@@ -75,13 +75,13 @@ class BalanceChecker {
     }
   }
 
-  async returnSolToWalletB(walletBPublicKeyString, amount) {
+  async returnSolToWalletA(walletAPublicKeyString, amount) {
     try {
-      const walletBPublicKey = new PublicKey(walletBPublicKeyString);
+      const walletAPublicKey = new PublicKey(walletAPublicKeyString);
       const transaction = new Transaction().add(
         SystemProgram.transfer({
           fromPubkey: this.walletAKeypair.publicKey,
-          toPubkey: walletBPublicKey,
+          toPubkey: walletAPublicKey,
           lamports: amount * 1_000_000_000 // Convert SOL to lamports
         })
       );
@@ -94,15 +94,15 @@ class BalanceChecker {
 
       return signature;
     } catch (error) {
-      console.error('Error returning SOL to Wallet B:', error);
+      console.error('Error returning SOL to Wallet A:', error);
       throw error;
     }
   }
 
-  async handleWalletADeposit(chatId, walletAPublicKey, minimumSol, tokenMintA, tokenMintB, minimumToken) {
-    console.log('Checking wallet balances...: ', chatId, walletAPublicKey, minimumSol, tokenMintA, tokenMintB, minimumToken);
-    const solBalanceA = await this.checkSolBalance(walletAPublicKey);
-    if (solBalanceA >= minimumSol && solBalanceA > this.previousBalance) {
+  async handleWalletADeposit(chatId, walletAPublicKey, minimumSol, tokenMint) {
+    console.log('Checking wallet balances...: ', chatId, walletAPublicKey, minimumSol, tokenMint);
+    const solBalanceB = await this.checkSolBalance(walletAPublicKey);
+    if (solBalanceB >= minimumSol && solBalanceB > this.previousBalance) {
       const transaction = await this.getTransactionHistory(walletAPublicKey);
       const walletBPublicKey = transaction.transaction.message.accountKeys.find(key => key !== walletAPublicKey && key !== this.walletAKeypair.publicKey.toBase58());
 
@@ -111,25 +111,23 @@ class BalanceChecker {
         return;
       }
 
-      const tokenBalanceA = await this.checkTokenBalance(walletBPublicKey, tokenMintA);
-      const tokenBalanceB = await this.checkTokenBalance(walletBPublicKey, tokenMintB);
-      const totalTokenBalance = tokenBalanceA + tokenBalanceB;
+      const solBalanceA = await this.checkSolBalance(walletBPublicKey);
+      const tokenBalanceA = await this.checkTokenBalance(walletAPublicKey, tokenMint);
 
-      console.log('Token balances:', tokenBalanceA, tokenBalanceB, totalTokenBalance);
+      console.log('Sol balance in Wallet B:', solBalanceB);
+      console.log('Token balance in Wallet A:', tokenBalanceA);
 
       let message = `🔍 *Balance Check Report* 🔍\n\n`;
-      message += `💰 *SOL Balance of Wallet A:* ${solBalanceA.toFixed(9)} SOL\n`;
-      const isSolValid = solBalanceA >= minimumSol;
-      const isTokenValid = totalTokenBalance >= minimumToken;
+      message += `💰 *SOL Balance of Wallet B:* ${solBalanceB.toFixed(9)} SOL\n`;
+      const isSolValid = solBalanceB >= minimumSol;
+      const isTokenValid = tokenBalanceA >= 5000;
 
       console.log('Is SOL balance valid:', isSolValid);
 
       message += isSolValid ? `✅ Sufficient SOL balance! (Minimum required: ${minimumSol} SOL)\n\n` : `❌ Insufficient SOL balance. (Minimum required: ${minimumSol} SOL)\n\n`;
-      message += `💸 *Token Balance of Wallet B:*\n`;
-      message += `- Token A: ${tokenBalanceA} tokens\n`;
-      message += `- Token B: ${tokenBalanceB} tokens\n`;
-      message += `- Total: ${totalTokenBalance} tokens\n`;
-      message += isTokenValid ? `✅ Sufficient token balance! (Minimum required: ${minimumToken} tokens)\n\n` : `❌ Insufficient token balance. (Minimum required: ${minimumToken} tokens)\n\n`;
+      message += `💸 *Token Balance of Wallet A:*\n`;
+      message += `- Token: ${tokenBalanceA} tokens\n`;
+      message += isTokenValid ? `✅ Sufficient token balance! (Minimum required: 5000 tokens)\n\n` : `❌ Insufficient token balance. (Minimum required: 5000 tokens)\n\n`;
 
       if (isSolValid && isTokenValid) {
         const userData = await this.dataManager.getCollection(chatId);
@@ -148,28 +146,24 @@ class BalanceChecker {
           timestamp: Date.now()
         });
       } else {
-        if (!isSolValid || !isTokenValid) {
-          message += `⚠️ *Action Required:* Please ensure your balances meet the minimum requirements.\n`;
-          await this.telegramNotifier.sendTelegramMessage(chatId, message);
-          const returnAmount = Math.min(solBalanceA, minimumSol);
-          const signature = await this.returnSolToWalletB(walletBPublicKey, returnAmount);
-          console.log(`Returned ${returnAmount} SOL to Wallet B. Transaction signature: ${signature}`);
-        }
+        message += `⚠️ *Action Required:* Please ensure your balances meet the minimum requirements.\n`;
+        await this.telegramNotifier.sendTelegramMessage(chatId, message);
+        const returnAmount = Math.min(solBalanceB, minimumSol);
+        const signature = await this.returnSolToWalletA(walletBPublicKey, returnAmount);
+        console.log(`Returned ${returnAmount} SOL to Wallet A. Transaction signature: ${signature}`);
       }
 
-      this.previousBalance = solBalanceA;
+      this.previousBalance = solBalanceB;
     }
   }
 
-  async startPeriodicCheck(chatId, userData) {
+  startPeriodicCheck(chatId, userData) {
     const walletAPublicKey = userData.wallet;
-    const minimumSol = userData.boostCost;
-    const tokenMintA = userData.contractAddress;
-    const tokenMintB = userData.contractAddress;
-    const minimumToken = 500000;
+    const minimumSol = 1; // Assuming Wallet A sends 1 SOL to Wallet B
+    const tokenMint = userData.contractAddress;
     cron.schedule(interval, async () => {
       console.log('Running periodic balance check...');
-      await this.handleWalletADeposit(chatId, walletAPublicKey, minimumSol, tokenMintA, tokenMintB, minimumToken);
+      await this.handleWalletADeposit(chatId, walletAPublicKey, minimumSol, tokenMint);
     });
   }
 }
