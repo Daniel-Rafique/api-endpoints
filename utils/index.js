e3a90ccc3252d8785617bf228a889bdd4c4673b7
Daniel-Rafique <drafique@tfd.ie>
2024-08-01 19:07:38 +0100

    Qeue retry attampts

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,9 @@
 function escapeMarkdown(text) {
   if (!text || typeof text !== 'string') return text;
-  return text.replace(/[_*[\]()~`>#+\-=|{}.!]/g, '\\$&');
+  return text.replace(/[_*[\]()~`>#+\-=|{}.!]/g, '\\$&')
+             .replace(/\\/g, '\\\\')
+             .replace(/\(/g, '\\(')
+             .replace(/\)/g, '\\)');
 }
 
 module.exports = {
