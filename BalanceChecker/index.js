9df349dc90140515d3ca97801f7c98e7e535fb2f
Daniel-Rafique <drafique@tfd.ie>
2024-08-01 12:34:37 +0100

    Update messages

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -188,7 +188,6 @@ class BalanceChecker {
     }
   }
 
-
   async sendTelegramMessage(chatId, text) {
     const cacheKey = `${chatId}`;
     if (this.messageCache[cacheKey] !== text) {
@@ -199,7 +198,7 @@ class BalanceChecker {
     }
   }
 
-  startPeriodicCheck(chatId, walletAPublicKeyString, minimumSolBalance, tokenMintAddress) {
+  startPeriodicCheck(chatId, walletAPublicKeyString, minimumSolBalance, minimumTokenBalance, tokenMintAddress) {
     cron.schedule('*/1 * * * *', async () => {
       console.log('Running periodic balance check...');
       await this.runBalanceCheck(chatId, walletAPublicKeyString, minimumSolBalance, minimumTokenBalance, tokenMintAddress);
