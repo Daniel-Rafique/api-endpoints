7112abdec335e53605184aaf2a556fb85b1b682d
Daniel-Rafique <drafique@tfd.ie>
2024-08-01 14:29:02 +0100

    Parse Markdown

--- constants/index.js
+++ constants/index.js
@@ -2,12 +2,12 @@ const { escapeMarkdown } = require('../utils');
 
 const MESSAGES = {
   BALANCE_CHECK_REPORT: "🔍 *Balance Check Report:*",
-  SOL_BALANCE_A: (solBalance) => `\n💰 SOL Balance is: ${solBalance.toFixed(9)} SOL`,
-  TOKEN_BALANCE_B: (tokenBalance) => `\n🪙 Token Balance of sender: ${tokenBalance.toFixed(9)} tokens`,
-  INSUFFICIENT_SOL: (minimumSolBalance) => `\n❌ The SOL balance does not meet the required minimum of ${minimumSolBalance} SOL.`,
-  INSUFFICIENT_TOKEN: (minimumTokenBalance) => `\n❌ The Token balance does not meet the required minimum of ${minimumTokenBalance} tokens.`,
+  SOL_BALANCE_A: (solBalance) => `\n💰 SOL Balance is: ${escapeMarkdown(solBalance.toFixed(2))} SOL\\.`,
+  TOKEN_BALANCE_B: (tokenBalance) => `\n🪙 Token Balance of sender: ${escapeMarkdown(tokenBalance.toFixed(9))} tokens\\.`,
+  INSUFFICIENT_SOL: (minimumSolBalance) => `\n❌ The SOL balance does not meet the required minimum of ${escapeMarkdown(minimumSolBalance)} SOL\\.`,
+  INSUFFICIENT_TOKEN: (minimumTokenBalance) => `\n❌ The Token balance does not meet the required minimum of ${escapeMarkdown(minimumTokenBalance)} tokens\\.`,
   SUFFICIENT_BALANCE: "\n✅ Both balances meet the required minimums.",
-  RETURNED_SOL: (solBalance, transactionSignature) => `\n🔄 Returned ${solBalance.toFixed(9)} SOL to sender. Transaction signature: [${transactionSignature}](https://solscan.io/tx/${transactionSignature})`,
+  RETURNED_SOL: (solBalance, transactionSignature) => `\n🔄 Returned ${escapeMarkdown(solBalance.toFixed(2))} SOL to sender. Transaction signature: ${escapeMarkdown(transactionSignature)}`,
   ERROR_DURING_CHECK: (errorMessage) => `\n❌ Error during balance check: ${errorMessage}`
 };
 
