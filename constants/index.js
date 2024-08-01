98479cfd12a24688e188c1854b7f763a2e6d08c8
Daniel-Rafique <drafique@tfd.ie>
2024-08-01 11:31:46 +0100

    Finally got SPL Token balance

--- constants/index.js
+++ constants/index.js
@@ -3,11 +3,11 @@ const { escapeMarkdown } = require('../utils');
 module.exports = {
   MESSAGES: {
     BALANCE_CHECK_REPORT: '🔍 *Balance Update* 🔍\n\n',
-    SOL_BALANCE_A: (solBalance) => `💰 SOL Balance is: ${solBalance.toFixed(9)} SOL\n`,
+    SOL_BALANCE_A: (solBalance) => `💰 Your Balance is: ${solBalance.toFixed(9)} SOL\n`,
     TOKEN_BALANCE_B: (tokenBalance) => `🪙 Token Balance of sender: ${tokenBalance} tokens\n`,
     SUFFICIENT_BALANCE: '✅ Both SOL balance and Token balance are sufficient - activating...\n',
-    INSUFFICIENT_SOL: (minimumSolBalance) => `❌ The SOL balance does not meet the required minimum of ${minimumSolBalance} SOL.\n`,
-    INSUFFICIENT_TOKEN: (minimumTokenBalance) => `❌ The Token balance does not meet the required minimum of ${minimumTokenBalance} tokens.\n`,
+    INSUFFICIENT_SOL: (minimumSolBalance) => `❌ Your SOL balance does not meet the required minimum of ${minimumSolBalance} SOL.\n`,
+    INSUFFICIENT_TOKEN: (minimumTokenBalance) => `❌ Sender Token balance does not meet the required minimum of ${minimumTokenBalance} tokens.\n`,
     RETURNED_SOL: (solBalance, signature) => `🔄 Returned ${solBalance} SOL to sender. Transaction signature: ${escapeMarkdown(signature)}\n`,
     ERROR_DURING_CHECK: (errorMessage) => `❌ Error during balance check: ${escapeMarkdown(errorMessage)}\n`,
   },
