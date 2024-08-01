d01080d1e1e2e09437d706e86fc4b11158df68b0
Daniel-Rafique <drafique@tfd.ie>
2024-08-01 04:28:55 +0100

    Fetch SPL Token Balance

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -62,22 +62,22 @@ class BalanceChecker {
       console.log('Checking token balance...');
       console.log('Wallet Public Key:', walletPublicKeyString);
       console.log('Token Mint Address:', tokenMintAddress);
-
+  
       if (!isValidPublicKey(walletPublicKeyString)) {
         throw new Error(`Invalid public key input: ${walletPublicKeyString}`);
       }
-
+  
       if (!isValidPublicKey(tokenMintAddress)) {
         throw new Error(`Invalid token mint address input: ${tokenMintAddress}`);
       }
-
+  
       // Use the connection from the class instance
       const { connection } = this;
-
+  
       // Convert the public key string to a PublicKey object
       const walletPublicKey = new PublicKey(walletPublicKeyString);
       console.log('Public Key object created:', walletPublicKey.toString());
-
+  
       // Get the associated token address for the SPL token
       const associatedTokenAddress = await getAssociatedTokenAddress(
         ASSOCIATED_TOKEN_PROGRAM_ID,
@@ -85,23 +85,30 @@ class BalanceChecker {
         new PublicKey(tokenMintAddress),
         walletPublicKey
       );
-
+  
       console.log('Associated Token Address:', associatedTokenAddress.toString());
-
+  
       // Fetch the token account balance
-      const tokenAccountInfo = await connection.getParsedAccountInfo(associatedTokenAddress);
+      const tokenAccountInfo = await connection.getAccountInfo(associatedTokenAddress);
       console.log('Token Account Info:', tokenAccountInfo);
-
+  
       // Check if the account exists and get the balance
       let tokenBalance = 0;
-      if (tokenAccountInfo.value) {
-        const tokenAmount = tokenAccountInfo.value.data.parsed.info.tokenAmount;
-        tokenBalance = tokenAmount.uiAmount;
-        console.log('Token Amount:', tokenAmount);
+      if (tokenAccountInfo) {
+        const accountData = tokenAccountInfo.data;
+        const dataLayout = BufferLayout.struct([
+          BufferLayout.u8('accountType'),
+          BufferLayout.blob(32, 'mint'),
+          BufferLayout.blob(32, 'owner'),
+          BufferLayout.nu64('amount'),
+        ]);
+        const decodedData = dataLayout.decode(accountData);
+        tokenBalance = decodedData.amount / Math.pow(10, 6); // Adjust for the token's decimal places
+        console.log('Decoded Data:', decodedData);
       } else {
         console.log('Token account does not exist or no data found.');
       }
-
+  
       console.log('Token Balance: ', tokenBalance);
       return tokenBalance;
     } catch (error) {
@@ -109,6 +116,7 @@ class BalanceChecker {
       throw error;
     }
   }
+  
 
   async sendTelegramMessage(chatId, text) {
     await this.telegramNotifier.sendTelegramMessage(chatId, text);
