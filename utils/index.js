789342839d5bce0527a8ff333e572265c71f2abc
Daniel-Rafique <drafique@tfd.ie>
2024-08-01 14:59:40 +0100

    Parse Markdown

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 function escapeMarkdown(text) {
   if (!text || typeof text !== 'string') return text;
-  return text.replace(/([_*[\]()~`>#+-=|{}.!\\])/g, '\\$1');
-}  
+  return text.replace(/([_*[\]()~`>#+-=|{}.!])/g, '\\$1');
+}
   module.exports = {
     escapeMarkdown,
   };
