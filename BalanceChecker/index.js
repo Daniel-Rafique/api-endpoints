4fae766c2176daf1c476860a6cef9db419022c3d
Daniel-Rafique <drafique@tfd.ie>
2024-08-02 00:06:13 +0100

    Message cache refactor

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -230,7 +230,7 @@ worker.on('completed', async (job, result) => {
     new TelegramNotifier(process.env.TELEGRAM_TOKEN),
     result.walletAPrivateKey
   );
-  await balanceChecker.sendTelegramMessage(result.chatId, message);
+  await balanceChecker.sendTelegramMessage(chatId, message);
 });
 
 worker.on('failed', (job, err) => {
