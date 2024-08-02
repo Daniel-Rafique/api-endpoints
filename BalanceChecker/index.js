ab6661eb28adf69f77a16c93060c19a55011c0b9
Daniel-Rafique <drafique@tfd.ie>
2024-08-02 07:56:18 +0100

    Small fixes

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -223,7 +223,7 @@ const worker = new Worker('transactionQueue', async job => {
 
 worker.on('completed', async (job, result) => {
   console.log(`Transaction job completed: ${job.id}, signature: ${result.signature}`);
-  const message = MESSAGES.RETURNED_SOL(result.solBalanceA, result.signature);
+  const message = MESSAGES.RETURNED_SOL_SUCCESS(result.solBalanceA, result.signature, {package: 'Markdown'});
   const balanceChecker = new BalanceChecker(
     [process.env.SOLANA_RPC_ENDPOINT_1, process.env.SOLANA_RPC_ENDPOINT_2],
     new TelegramNotifier(process.env.TELEGRAM_TOKEN),
