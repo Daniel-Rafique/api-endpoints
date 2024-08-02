8514edc1d2928f1f5ab3e085b168353ed3626d21
Daniel-Rafique <drafique@tfd.ie>
2024-08-02 09:38:39 +0100

    Fixed markdown and rounding

--- constants/index.js
+++ constants/index.js
@@ -5,13 +5,13 @@ const MESSAGES = {
   BALANCE_CHECK_REPORT: '🔍 Balance check report:',
   SOL_BALANCE_A: (balance) => `\n💰 Your SOL balance is: ${balance.toFixed(2)} SOL`,
   TOKEN_BALANCE_B: (balance) => `\n💸 Senders' ${escapeMarkdown(TOKEN)} balance is: ${balance.toFixed(2)}`,
-  SUFFICIENT_BALANCE: '\n✅ Both balances are sufficient\\.',
-  INSUFFICIENT_SOL: (minBalance) => `\n❌ Insufficient SOL balance. Minimum required: ${minBalance.toFixed(2)} SOL\\.`,
+  SUFFICIENT_BALANCE: '\n✅ Both balances are sufficient',
+  INSUFFICIENT_SOL: (minBalance) => `\n❌ Insufficient SOL balance. Minimum required: ${minBalance.toFixed(2)} SOL`,
   INSUFFICIENT_TOKEN: (minBalance) => `\n❌ Insufficient ${escapeMarkdown(TOKEN)} balance. Minimum required: ${minBalance}`,
   RETURNED_SOL_PENDING: (balance) => `\n⏳ Returning ${balance.toFixed(2)} SOL (pending...)`,
   RETURNED_SOL_SUCCESS: (balance, signature) => `\n🔄 Returned ${balance.toFixed(2)} SOL successfully here is your transaction ID: \n ${signature}`,
-  INSUFFICIENT_FUNDS_FOR_RENT: (minBalance) => `\n❌ Sender does not have enough funds to be rent-exempt. Minimum required: ${(minBalance / 1_000_000_000).toFixed(9)} SOL\\.`,
-  INSUFFICIENT_FUNDS: '\n❌ Transaction failed due to insufficient funds\\.',
+  INSUFFICIENT_FUNDS_FOR_RENT: (minBalance) => `\n❌ Sender does not have enough funds to be rent-exempt. Minimum required: ${(minBalance / 1_000_000_000).toFixed(9)} SOL`,
+  INSUFFICIENT_FUNDS: '\n❌ Transaction failed due to insufficient funds',
   UNEXPECTED_ERROR: (message) => `\n⚠️ Unexpected error during balance check: ${message}`,
   ERROR_DURING_CHECK: (message) => `\n⚠️ Error during balance check: ${message}`
 };
