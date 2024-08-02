4bc267d2cafcb51ed32400beeaf49a2f73b14435
Daniel-Rafique <drafique@tfd.ie>
2024-08-02 08:07:18 +0100

    Small fixes

--- constants/index.js
+++ constants/index.js
@@ -4,7 +4,7 @@ const TOKEN = process.env.TOKEN;
 const MESSAGES = {
   BALANCE_CHECK_REPORT: '🔍 Balance check report:',
   SOL_BALANCE_A: (balance) => `\n💰 Your SOL balance is: ${balance.toFixed(9)} SOL`,
-  TOKEN_BALANCE_B: (balance) => `\n💸 ${escapeMarkdown(TOKEN)} balance of sender is: ${balance}`,
+  TOKEN_BALANCE_B: (balance) => `\n💸 Sender ${escapeMarkdown(TOKEN)} balance is: ${balance}`,
   SUFFICIENT_BALANCE: '\n✅ Both balances are sufficient.',
   INSUFFICIENT_SOL: (minBalance) => `\n❌ Insufficient SOL balance. Minimum required: ${minBalance.toFixed(9)} SOL.`,
   INSUFFICIENT_TOKEN: (minBalance) => `\n❌ Insufficient ${escapeMarkdown(TOKEN)} balance. Minimum required: ${minBalance}`,
