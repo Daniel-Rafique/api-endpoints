d6fa1367f07d8f0a6020ba8bbc7642b4cc6d8e57
Daniel-Rafique <drafique@tfd.ie>
2024-08-02 09:13:04 +0100

    Sol being returned successfully each time

--- constants/index.js
+++ constants/index.js
@@ -3,13 +3,13 @@ const TOKEN = process.env.TOKEN;
 
 const MESSAGES = {
   BALANCE_CHECK_REPORT: '🔍 Balance check report:',
-  SOL_BALANCE_A: (balance) => `\n💰 Your SOL balance is: ${balance.toFixed(9)} SOL`,
+  SOL_BALANCE_A: (balance) => `\n💰 Your SOL balance is: ${balance.toFixed(2)} SOL`,
   TOKEN_BALANCE_B: (balance) => `\n💸 Senders' ${escapeMarkdown(TOKEN)} balance is: ${balance}`,
   SUFFICIENT_BALANCE: '\n✅ Both balances are sufficient.',
-  INSUFFICIENT_SOL: (minBalance) => `\n❌ Insufficient SOL balance. Minimum required: ${minBalance.toFixed(9)} SOL.`,
+  INSUFFICIENT_SOL: (minBalance) => `\n❌ Insufficient SOL balance. Minimum required: ${minBalance.toFixed(2)} SOL.`,
   INSUFFICIENT_TOKEN: (minBalance) => `\n❌ Insufficient ${escapeMarkdown(TOKEN)} balance. Minimum required: ${minBalance}`,
-  RETURNED_SOL_PENDING: (balance) => `\n⏳ Returning ${balance.toFixed(9)} SOL (pending...)`,
-  RETURNED_SOL_SUCCESS: (balance, signature) => `\n🔄 Returned ${balance.toFixed(9)} SOL successfully. \nTransaction signature: ${signature}`,
+  RETURNED_SOL_PENDING: (balance) => `\n⏳ Returning ${balance.toFixed(2)} SOL (pending...)`,
+  RETURNED_SOL_SUCCESS: (balance, signature) => `\n🔄 Returned ${balance.toFixed(2)} SOL successfully. \nTransaction ID: ${signature}`,
   INSUFFICIENT_FUNDS_FOR_RENT: (minBalance) => `\n❌ Sender does not have enough funds to be rent-exempt. Minimum required: ${(minBalance / 1_000_000_000).toFixed(9)} SOL.`,
   INSUFFICIENT_FUNDS: '\n❌ Transaction failed due to insufficient funds.',
   UNEXPECTED_ERROR: (message) => `\n⚠️ Unexpected error during balance check: ${message}`,
