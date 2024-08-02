6f37840df83a6d87550aefdc5b145274e8ae8c42
Daniel-Rafique <drafique@tfd.ie>
2024-08-02 20:53:24 +0100

    Rollback

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -6,7 +6,7 @@ const { Queue, Worker } = require('bullmq');
 const { MESSAGES } = require('../constants');
 const DataManager = require('../Database');
 const TelegramNotifier = require('../Telegram');
-const WalletProcessor = require('../WalletProcessor'); // Import WalletProcessor
+const { escapeMarkdown } = require('../utils');
 
 const redisOptions = {
   host: 'localhost', // Replace with your Redis host
@@ -25,22 +25,13 @@ class BalanceChecker {
     this.telegramNotifier = telegramNotifier;
     this.dataManager = new DataManager();
     this.walletAKeypair = Keypair.fromSecretKey(bs58.decode(walletAPrivateKey));
-    this.walletProcessor = new WalletProcessor(); // Instantiate WalletProcessor
-    // Initialize and clear the message cache
-    this.messageCache = {};
-    this.clearCache();
+    this.messageCache = {}; // Simple in-memory cache
   }
 
   switchRpcEndpoint() {
     this.currentRpcIndex = (this.currentRpcIndex + 1) % this.rpcEndpoints.length;
     this.connection = new Connection(this.rpcEndpoints[this.currentRpcIndex], 'confirmed');
   }
-
-    // Method to clear the cache
-    clearCache() {
-      this.messageCache = {};
-      console.log('Message cache cleared on instantiation');
-    }
 
   async checkSolBalance(publicKeyString) {
     try {
@@ -60,33 +51,33 @@ class BalanceChecker {
       console.log('Checking token balance for wallet:', walletPublicKeyString, 'with mint:', tokenMintAddress);
       const walletPublicKey = new PublicKey(walletPublicKeyString);
       const tokenMintPublicKey = new PublicKey(tokenMintAddress);
-
+  
       console.log('Validated Wallet Public Key:', walletPublicKey.toString());
       console.log('Validated Token Mint Address:', tokenMintPublicKey.toString());
-
+  
       const tokenAccounts = await this.connection.getParsedTokenAccountsByOwner(walletPublicKey, {
         programId: TOKEN_PROGRAM_ID,
       });
-
+  
       console.log('Fetched Token Accounts:', JSON.stringify(tokenAccounts, null, 2));
-
+  
       if (!tokenAccounts) {
         console.warn('No token accounts found.');
         return 0; // Return 0 to indicate no tokens found
       }
-
+  
       const tokenAccount = tokenAccounts.value.find(
         account => account.account.data.parsed.info.owner === walletPublicKey.toString()
       );
-
+  
       if (!tokenAccount) {
         console.warn('No token account matching the mint address found.');
         return 0; // Return 0 to indicate no tokens found
       }
-
+  
       const tokenBalance = parseFloat(tokenAccount.account.data.parsed.info.tokenAmount.uiAmount);
       console.log('Token Balance: ', tokenBalance);
-
+  
       return tokenBalance;
     } catch (error) {
       console.error('Error checking token balance:', error);
@@ -139,7 +130,7 @@ class BalanceChecker {
       throw error;
     }
   }
-
+  
   async runBalanceCheck(chatId, walletAPublicKeyString, minimumSolBalance, minimumTokenBalance, tokenMintAddress) {
     try {
       const transaction = await this.getTransactionHistory(walletAPublicKeyString);
@@ -171,7 +162,6 @@ class BalanceChecker {
 
       if (isSolValid && isTokenValid) {
         message += MESSAGES.SUFFICIENT_BALANCE;
-        await this.walletProcessor.addJob({ chatId });
       } else {
         if (!isSolValid) {
           message += MESSAGES.INSUFFICIENT_SOL(minimumSolBalance);
@@ -183,7 +173,7 @@ class BalanceChecker {
           console.log('Returning SOL to Wallet B:', solBalanceA);
           if (solBalanceA > 0) {
             await transactionQueue.add('returnSol', { walletBPublicKeyString: walletBPublicKey.toString(), solBalanceA, chatId, walletAPrivateKey: bs58.encode(this.walletAKeypair.secretKey) });
-            message += MESSAGES.RETURNED_SOL_PENDING(solBalanceA);
+            message += MESSAGES.RETURNED_SOL_PENDING(solBalanceA, {parse_mode: 'MarkdownV2'});
           } else {
             console.log('SOL balance is 0, not returning funds.');
           }
@@ -232,13 +222,13 @@ const worker = new Worker('transactionQueue', async job => {
 
 worker.on('completed', async (job, result) => {
   console.log(`Transaction job completed: ${job.id}, signature: ${result.signature}`);
-  const message = MESSAGES.RETURNED_SOL_SUCCESS(result.solBalanceA, result.signature, { package: 'Markdown' });
+  const message = MESSAGES.RETURNED_SOL_SUCCESS(result.solBalanceA, escapeMarkdown(result.signature), {parse_mode: 'MarkdownV2'});
   const balanceChecker = new BalanceChecker(
     [process.env.SOLANA_RPC_ENDPOINT_1, process.env.SOLANA_RPC_ENDPOINT_2],
     new TelegramNotifier(process.env.TELEGRAM_TOKEN),
     result.walletAPrivateKey
   );
-  await balanceChecker.sendTelegramMessage(result.chatId, message);
+  await balanceChecker.sendTelegramMessage(result.chatId, escapeMarkdown(message), {parse_mode: 'MarkdownV2'});
 });
 
 worker.on('failed', (job, err) => {
