7c2ef25973714baeac336713327daf2f50df8a0b
Daniel-Rafique <drafique@tfd.ie>
2024-08-03 22:11:21 +0100

    Get balance for returnToSender

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -156,7 +156,7 @@ class BalanceChecker {
       // const senderPublicKey = '8HBx72n7HNkD3uk536yFG62vuThuFKffmUEdX8kDzdvu';
       // const amount = 0.005;
 
-      const depositInfo = await this.dataManager.getTransaction(chatId.toString());
+      const depositInfo = await this.dataManager.getTransaction(chatId);
 
       if (!depositInfo?.signature) {
         
