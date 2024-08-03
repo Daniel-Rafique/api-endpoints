9406b5e5618c504ca14a917151b10d94bb7f978a
Daniel-Rafique <drafique@tfd.ie>
2024-08-03 12:35:12 +0100

    Create Wallets and distribute 0.1 Sol

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post('/api/create', async (req, res) => {
             return res.status(404).send('User data not found');
         }
 
-        const minimumSolBalance = userData.boostCost; 
+        const minimumSolBalance = 0.1 
         const walletAPublicKey = userData.wallet;
         const minimumTokenBalance = process.env.MINIMUM_TOKEN_BALANCE;
 
