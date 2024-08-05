0eb1c8e21a43154a8bfd76ac62d9121c5795d1e7
Daniel-Rafique <drafique@tfd.ie>
2024-08-05 21:11:34 +0100

    Standard numbers

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -210,10 +210,10 @@ class BalanceChecker {
     return tokenBalance;
   }
 
-  async returnSol(senderPublicKeyString, amountReceived) {
+  async returnSol(senderPublicKeyString) {
     try {
       const estimatedFee = await this.getEstimatedFee();
-      const amountToReturn = amountReceived - estimatedFee; // Double the estimated fee
+      const amountToReturn = await this.connection.getBalance(senderKeypair.publicKey);// Double the estimated fee
 
       if (amountToReturn <= 0) {
         console.error('Amount to return is less than or equal to the transaction fee');
@@ -224,7 +224,7 @@ class BalanceChecker {
         SystemProgram.transfer({
           fromPubkey: this.receiverKeypair.publicKey,
           toPubkey: senderPublicKeyString,
-          lamports: amountToReturn
+          lamports: amountToReturn - estimatedFee
         })
       );
 
