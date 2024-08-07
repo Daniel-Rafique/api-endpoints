0e6e356d13432c82e96f9d27a8993540a7ff30ca
Daniel-Rafique <drafique@tfd.ie>
2024-08-07 13:33:33 +0100

    Call distribute Solana after instance initialized

--- BalanceChecker/index.js
+++ BalanceChecker/index.js
@@ -236,8 +236,8 @@ class BalanceChecker {
       const amountReceived = transaction.meta.postBalances[receiverIndex] - transaction.meta.preBalances[receiverIndex];
   
       if (amountReceived <= 0) {
-        console.log('Received a transaction with no balance change, continuing...');
-        // Continue processing, do not return early
+        console.error('Invalid transaction amount');
+        return;
       }
   
       const senderPublicKeyString = new PublicKey(senderPublicKey);
