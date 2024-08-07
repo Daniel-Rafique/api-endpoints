1f4f5bac8c96ff0683f1eb5f35b66b1d966c239e
Daniel-Rafique <drafique@tfd.ie>
2024-08-07 09:22:04 +0100

    Update server.js execution flow

--- InstanceInitializer/index.js
+++ InstanceInitializer/index.js
@@ -19,7 +19,7 @@ class InstanceInitializer {
 
     this.firestore = new Firestore({
       projectId: 'koynlabs-2f749',
-      keyFilename: '.config/firebaseServiceAccountKey.json'
+      keyFilename: path.join(os.homedir(), ENV_PATH, '.config/firebaseServiceAccountKey.json'),
     });
   }
 
