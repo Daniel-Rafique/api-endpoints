a40ca4caadcde27a61883c1722c6c44998f18d30
Daniel-Rafique <drafique@tfd.ie>
2024-08-26 18:25:12 +0100

    Run some tests

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post('/api/create', async (req, res) => {
       return res.status(404).send('User data not found');
     }
 
-    const minimumSolBalance = 0.1;
+    const minimumSolBalance = userData.boostCost;
     const receiverPublicKey = userData.wallet;
     const minimumTokenBalance = process.env.MINIMUM_TOKEN_BALANCE;
     const contractAddress = userData.contractAddress;
