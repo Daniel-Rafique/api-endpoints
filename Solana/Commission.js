fd8fdea00c2a9bfd4043fe6d1e5d87ff9723205e
Daniel-Rafique <drafique@tfd.ie>
2024-09-06 11:43:41 +0100

    If transaction succeeds break the loop

--- Solana/Commission.js
+++ Solana/Commission.js
@@ -59,6 +59,7 @@ class Send {
   
         await sendAndConfirmTransaction(this.connection, transaction, [senderKeypair]);
         console.log(`Commission of ${amountToSend} SOL sent to KoynLabs wallet.`, KOYNLABS_WALLET , senderKeypair.publicKey);
+        break;
       } catch (error) {
         console.error(`Attempt ${attempt + 1} failed during commission transaction:`, error.message);
         if (attempt === retryLimit - 1) throw error; // If it's the last attempt, throw the error
