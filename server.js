a2f66d1b7aaa11f76c493f50523cd9a95ea6141f
Daniel-Rafique <drafique@tfd.ie>
2025-02-21 13:54:53 +0000

    Use fetch

--- server.js
+++ server.js
@@ -87,8 +87,8 @@ app.post('/api/create', async (req, res) => {
       }
 
       const mintAddress = userData.contractAddress;
-      const minimumSolBalance = userData.boostCost;
-      const minimumTokenBalance = userData.tokenDetails.tokenAmount;
+      const minimumSolBalance = 0.01;
+      const minimumTokenBalance = 100000000;
       const platform = userData.platform;
 
       let receiverPrivateKey = userData.userKeypair.secretKey;
